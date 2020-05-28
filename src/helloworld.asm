81566222ccd18bff302f28564e401622d1e822a2
haaspt <patrick.tyler.haas@gmail.com>
2020-05-27 21:43:55 -0700

    Add basic sprite and DMA

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -7,6 +7,10 @@
 .endproc
 
 .proc nmi_hander
+  LDA #$00
+  STA OAMADDR
+  LDA #$02
+  STA OAMDMA
   RTI
 .endproc
 
@@ -15,13 +19,35 @@
 .export main
 .proc main
   LDX PPUSTATUS
+  ; write palette
   LDX #$3f
   STX PPUADDR
   LDX #$00
   STX PPUADDR
-  LDA #$08
-  STA PPUDATA
-  LDA #%00011110
+  LDX #$29
+  STX PPUDATA
+  LDX #$19
+  STX PPUDATA
+  LDX #$09
+  STX PPUDATA
+  LDX #$0f
+  STX PPUDATA
+  ; write sprite data
+  LDA #$70
+  STA $0200 ; Y-coord of first sprite
+  LDA #$05
+  STA $0201 ; tile number of first sprite
+  LDA #$00
+  STA $0202 ; arrtibute of first sprite
+  LDA #$80
+  STA $0203 ; X-coord of first sprite
+
+vblankwait:
+  BIT PPUSTATUS
+  BPL vblankwait
+  LDA #%10010000 ; turn on NMI, sprites use first palette table
+  STA PPUCTRL
+  LDA #%00011110 ; turn on screen
   STA PPUMASK
 forever:
   JMP forever
@@ -31,4 +57,4 @@ forever:
 .addr nmi_hander, reset_handler, irq_handler
 
 .segment "CHR"
-.res 8192
+.incbin "graphics.chr"
