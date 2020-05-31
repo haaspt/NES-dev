c69cadac6644c9ad802f9d67f745fb0b45e93130
haaspt <patrick.tyler.haas@gmail.com>
2020-05-30 20:06:41 -0700

    Add zeropage ram reset

--- src/reset.asm
+++ src/reset.asm
@@ -9,6 +9,20 @@
   LDX #$00
   STX PPUCTRL
   STX PPUMASK
+  ; Clear zerotable
+  LDA #$00
+clear_loop:
+  STA $000, X
+  STA $100, X
+  STA $200, X
+  STA $300, X
+  STA $400, X
+  STA $500, X
+  STA $600, X
+  STA $700, X
+  INX
+  CPX #$00
+  BNE clear_loop
 vblankwait:
   BIT PPUSTATUS
   BPL vblankwait
